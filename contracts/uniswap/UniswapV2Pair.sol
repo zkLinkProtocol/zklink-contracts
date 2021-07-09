d96c4d609a741efff8b11bd60b4244b6b81f0527
benny <benny@zklink.org>
2021-07-09 14:11:11 +0800

    remove unused contract file of uniswap

--- contracts/uniswap/UniswapV2Pair.sol
+++ contracts/uniswap/UniswapV2Pair.sol
@@ -2,11 +2,7 @@ pragma solidity =0.5.16;
 
 import './interfaces/IUniswapV2Pair.sol';
 import './UniswapV2ERC20.sol';
-import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
-import './interfaces/IUNISWAPERC20.sol';
-import './interfaces/IUniswapV2Factory.sol';
-import './interfaces/IUniswapV2Callee.sol';
 
 contract UniswapV2Pair is IUniswapV2Pair, UniswapV2ERC20 {
     using UniswapSafeMath  for uint;
--- contracts/uniswap/interfaces/IUniswapV2Callee.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity >=0.5.0;
-
-interface IUniswapV2Callee {
-    function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external;
-}
