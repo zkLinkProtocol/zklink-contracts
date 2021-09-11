f28a07d9cd66be3f8c6655b84a3a60293871c56c
benny <benny@zklink.org>
2021-09-11 15:05:11 +0800

    zkl add permit

--- contracts/ZKL.sol
+++ contracts/ZKL.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "openzeppelin-solidity/contracts/access/AccessControlEnumerable.sol";
 
 /**
  * @title ZKL token contract
  */
-contract ZKL is AccessControlEnumerable, ERC20Capped{
+contract ZKL is AccessControlEnumerable, ERC20Capped, ERC20Permit{
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     /**
@@ -18,7 +19,8 @@ contract ZKL is AccessControlEnumerable, ERC20Capped{
      *
      * See {ERC20-constructor}.
      */
-    constructor(string memory name_, string memory symbol_, uint256 cap, address networkGovernor, address zkLink) ERC20(name_, symbol_) ERC20Capped(cap) {
+    constructor(string memory name, string memory symbol, uint256 cap, address networkGovernor, address zkLink)
+        ERC20(name, symbol) ERC20Capped(cap) ERC20Permit(name) {
         _setupRole(DEFAULT_ADMIN_ROLE, networkGovernor);
 
         _setupRole(MINTER_ROLE, zkLink);
@@ -44,5 +46,12 @@ contract ZKL is AccessControlEnumerable, ERC20Capped{
      */
     function burn(uint256 amount) public virtual {
         _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev See {ERC20-_mint}.
+     */
+    function _mint(address account, uint256 amount) internal override(ERC20Capped, ERC20) {
+        ERC20Capped._mint(account, amount);
     }
 }
---
+++ test/zkl_test.js
@@ -0,0 +1,24 @@
+const hardhat = require('hardhat');
+const { expect } = require('chai');
+
+describe('ZKL unit tests', function () {
+    let zkl,wallet,networkGovernor,zkLink,alice;
+    beforeEach(async () => {
+        [wallet,networkGovernor,zkLink,alice] = await hardhat.ethers.getSigners();
+        const zklFactory = await hardhat.ethers.getContractFactory('ZKL');
+        zkl = await zklFactory.deploy('ZKLINK','ZKL',10000,networkGovernor.address,zkLink.address);
+    });
+
+    it('only address with MINTER_ROLE can mine', async () => {
+        await expect(zkl.connect(alice).mint(alice.address, 100)).to.be.reverted;
+        await expect(zkl.connect(networkGovernor).mint(alice.address, 100)).to.be.reverted;
+        await expect(zkl.connect(zkLink).mint(alice.address, 100)).to.be
+            .emit(zkl, 'Transfer')
+            .withArgs('0x0000000000000000000000000000000000000000', alice.address, 100);
+    });
+
+    it('mine can not over cap', async () => {
+        await zkl.connect(zkLink).mint(alice.address, 1000);
+        await expect(zkl.connect(zkLink).mint(alice.address, 9001)).to.be.revertedWith('ERC20Capped: cap exceeded');
+    });
+});
