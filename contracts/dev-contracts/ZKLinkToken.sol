ab1297e7c9f69bf1963f927d61af650fc78672b7
zkbenny <benny@zklink.org>
2023-06-14 17:56:58 +0800

    add ZKLinkToken for test

---
+++ contracts/dev-contracts/ZKLinkToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ZKLinkToken is ERC20 {
+
+    constructor () ERC20("ZKLink", "ZKL") {}
+}
