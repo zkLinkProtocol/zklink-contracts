fd4e74a978184ca900aa69992770670b9933e367
zkbenny <benny@zklink.org>
2023-06-14 17:59:19 +0800

    init total amount for ZKLinkToken

--- contracts/dev-contracts/ZKLinkToken.sol
+++ contracts/dev-contracts/ZKLinkToken.sol
@@ -6,5 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ZKLinkToken is ERC20 {
 
-    constructor () ERC20("ZKLink", "ZKL") {}
+    constructor () ERC20("ZKLink", "ZKL") {
+        _mint(msg.sender, 1000000000);
+    }
 }
