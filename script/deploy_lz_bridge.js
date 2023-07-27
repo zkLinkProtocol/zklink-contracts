0d3d2651c93ec060411593deb949153e67996afc
zkbenny <benny@zklink.org>
2023-07-27 18:56:34 +0800

    update lzbridge deploy script

--- script/deploy_lz_bridge.js
+++ script/deploy_lz_bridge.js
@@ -4,22 +4,16 @@ const logName = require('./deploy_log_name');
 const {layerZero} = require("./layerzero");
 
 task("deployLZBridge", "Deploy LayerZeroBridge")
-    .addParam("governor", "The governor address (default get from zkLink deploy log)", undefined, types.string, true)
     .addParam("zklink", "The zklink address (default get from zkLink deploy log)", undefined, types.string, true)
     .addParam("force", "Fore redeploy all contracts", false, types.boolean, true)
     .addParam("skipVerify", "Skip verify", false, types.boolean, true)
     .setAction(async (taskArgs, hardhat) => {
-        let governor = taskArgs.governor;
-        if (governor === undefined) {
-            governor = readDeployLogField(logName.DEPLOY_ZKLINK_LOG_PREFIX, logName.DEPLOY_LOG_GOVERNOR);
-        }
         let zklink = taskArgs.zklink;
         if (zklink === undefined) {
             zklink = readDeployLogField(logName.DEPLOY_ZKLINK_LOG_PREFIX, logName.DEPLOY_LOG_ZKLINK_PROXY);
         }
         let force = taskArgs.force;
         let skipVerify = taskArgs.skipVerify;
-        console.log('governor', governor);
         console.log('zklink', zklink);
         console.log('force redeploy all contracts?', force);
         console.log('skip verify contracts?', skipVerify);
@@ -37,11 +31,10 @@ task("deployLZBridge", "Deploy LayerZeroBridge")
         const {deployLogPath,deployLog} = createOrGetDeployLog(logName.DEPLOY_LZ_BRIDGE_LOG_PREFIX);
 
         deployLog[logName.DEPLOY_LOG_DEPLOYER] = contractDeployer.deployerWallet.address;
-        deployLog[logName.DEPLOY_LOG_GOVERNOR] = governor;
         fs.writeFileSync(deployLogPath, JSON.stringify(deployLog));
 
         // deploy lz bridge
-        let args = [governor, zklink, lzInfo.address];
+        let args = [zklink, lzInfo.address];
         let lzBridge;
         if (!(logName.DEPLOY_LOG_LZ_BRIDGE in deployLog) || force) {
             console.log('deploy layerzero bridge...');
