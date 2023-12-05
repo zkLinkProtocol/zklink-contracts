a0505a876cd7494d37a8850e93a01b79c75eabb3
zkbenny <benny@zklink.org>
2023-12-05 19:12:45 +0800

    add txNonce to bridge message

--- contracts/interfaces/ILineaL2Gateway.sol
+++ contracts/interfaces/ILineaL2Gateway.sol
@@ -5,22 +5,23 @@ import {IL2Gateway} from "./IL2Gateway.sol";
 import {ILineaGateway} from "./ILineaGateway.sol";
 
 interface ILineaL2Gateway is ILineaGateway, IL2Gateway {
-    event ClaimedDepositETH(bytes32 _zkLinkAddress, uint8 _subAccountId, uint256 _amount);
-    event ClaimedDepositERC20(address _token, uint104 _amount, bytes32 _zkLinkAddress, uint8 _subAccountId, bool _mapping);
+    event ClaimedDeposit(uint32 indexed _txNonce);
     event SetZkLink(address _zkLink);
 
     /// @notice Claim ETH callback from message service
+    /// @param _txNonce The deposit sequence of L1 gateway
     /// @param _zkLinkAddress The zkLink address deposited to
     /// @param _subAccountId The sub account id
     /// @param _amount The eth amount to deposit
-    function claimETHCallback(bytes32 _zkLinkAddress, uint8 _subAccountId, uint256 _amount) external payable;
+    function claimETHCallback(uint32 _txNonce, bytes32 _zkLinkAddress, uint8 _subAccountId, uint256 _amount) external payable;
 
     /// @notice Claim ERC20 callback from message service
+    /// @param _txNonce The deposit sequence of L1 gateway
     /// @param _isUSDC True when the native token is usdc
     /// @param _nativeToken The native token
     /// @param _amount The amount to deposit
     /// @param _zkLinkAddress The zkLink address deposited to
     /// @param _subAccountId The sub account id
     /// @param _mapping If receive a mapping token on zkLink
-    function claimERC20Callback(bool _isUSDC, address _nativeToken, uint256 _amount, bytes32 _zkLinkAddress, uint8 _subAccountId, bool _mapping) external;
+    function claimERC20Callback(uint32 _txNonce, bool _isUSDC, address _nativeToken, uint256 _amount, bytes32 _zkLinkAddress, uint8 _subAccountId, bool _mapping) external;
 }
