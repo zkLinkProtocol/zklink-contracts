873f9af12408201767f2eee939e92cf6e04a37dd
John <john@zklink.org>
2023-12-14 16:32:29 +0800

    delete useless code

--- zksync/hardhat.config.js
+++ zksync/hardhat.config.js
@@ -1,7 +1,5 @@
 const BaseConfig = require("../hardhat.base.config")
-// require("@matterlabs/hardhat-zksync-deploy")
 require("@matterlabs/hardhat-zksync-solc");
-// require("@matterlabs/hardhat-zksync-verify");
 
 module.exports = Object.assign({},BaseConfig, {
   network: {
