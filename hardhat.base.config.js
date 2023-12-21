ea706f456057571c0b533f2b650334301640ceb8
zkbenny <benny@zklink.org>
2023-12-21 09:58:18 +0800

    remove proxy code

--- hardhat.base.config.js
+++ hardhat.base.config.js
@@ -12,11 +12,6 @@ require("./script/deloy_multicall");
 require("./script/upgrade_l2_gateway");
 require("./script/upgrade_l1_gateway");
 
-const { ProxyAgent, setGlobalDispatcher } = require("undici")
-
-const proxyAgent = new ProxyAgent("http://127.0.0.1:1087")
-setGlobalDispatcher(proxyAgent);
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
