208f7ee28597cb5ba3728f6c02be32a694d09120
zkbenny <benny@zklink.org>
2023-12-22 18:36:14 +0800

    add gateway mainnet config

--- script/gateway.js
+++ script/gateway.js
@@ -19,4 +19,24 @@ module.exports = {
       "0xdfa112375c9be9d124932b1d104b73f888655329",
     ],
   },
+  ETH: {
+    LINEA: {
+      contractName: "LineaL1Gateway",
+      //   [messageService,tokenBridge,usdcBridge]
+      initializeParams: [
+        "0xd19d4B5d358258f05D7B411E21A1460D11B0876F",
+        "0x051F1D88f0aF5763fB888eC4378b4D8B29ea3319",
+        "0x504A330327A089d8364C4ab3811Ee26976d388ce",
+      ],
+    },
+  },
+  LINEA: {
+    contractName: "LineaL2Gateway",
+    //   [messageService,tokenBridge,usdcBridge]
+    initializeParams: [
+      "0x508Ca82Df566dCD1B0DE8296e70a96332cD644ec",
+      "0x353012dc4a9A6cF55c941bADC267f82004A8ceB9",
+      "0xA2Ee6Fce4ACB62D95448729cDb781e3BEb62504A",
+    ],
+  }
 };
